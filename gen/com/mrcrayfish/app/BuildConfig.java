84d57cb2d451a45e5b29434ee633975d704f3be1
MrCrayfish <mrcrayfish@hotmail.com>
2015-03-01 21:58:00 +1030

    Improved soundboard item layout

--- gen/com/mrcrayfish/app/BuildConfig.java
+++ gen/com/mrcrayfish/app/BuildConfig.java
@@ -1,7 +1,6 @@
 /** Automatically generated file. DO NOT MODIFY */
 package com.mrcrayfish.app;
 
-public final class BuildConfig
-{
-	public final static boolean DEBUG = true;
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
 }
